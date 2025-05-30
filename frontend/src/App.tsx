8ded90d23aabf84db13553de74b6bbe8746da651
John Lathrop <77928021+gigajohn@users.noreply.github.com>
2025-05-30 09:43:47 -0500

    add previous code

---
+++ frontend/src/App.tsx
@@ -0,0 +1,21 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import './App.css'
+import Home from './pages/Home.tsx'
+import Songs from './pages/Songs'
+import Navbar from './components/Navbar/Navbar'
+
+function App() { 
+  return (
+    <Router>
+      <div className='App'>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/songs" element={<Songs />} />
+        </Routes>
+      </div>
+    </Router>
+  )
+}
+
+export default App
