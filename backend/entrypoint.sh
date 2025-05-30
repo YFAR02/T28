50780f279ebdff31d60c9f163d9bc1ae9d5409b0
jjdubski <101855602+jjdubski@users.noreply.github.com>
2025-05-30 13:05:26 -0500

    fix frontend, backend config and docker setup

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 
-# if [ "$DATABASE" = "postgres" ]; then
-#     echo "Waiting for postgres..."
+if [ "$DATABASE" = "postgres" ]; then
+    echo "Waiting for postgres..."
 
-#     while ! nc -z $DATABASE_HOST $DATABASE_PORT; do
-#         echo "Waiting for postgres..."
-#     sleep 1
-#     done
+    while ! nc -z $DATABASE_HOST $DATABASE_PORT; do
+        echo "Waiting for postgres..."
+    sleep 1
+    done
 
-#     echo "PostgreSQL started"
-# fi
+    echo "PostgreSQL started"
+fi
 
 # Make migrations 
-python manage.py makemigrations
+# python manage.py makemigrations songs
+python manage.py makemigrations --noinput
 
 python manage.py migrate
 
