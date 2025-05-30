165a5582304efaece6799e5eaf095389e5fc33b3
YFAR02 <yfarooqui001@gmail.com>
2025-05-30 14:09:09 -0500

    login

---
+++ frontend/src/components/Navbar/login/login.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import './login.css';
+
+const Login: React.FC = () => {
+  return (
+    <div className="login-container">
+      <h2 className="login-title">Login</h2>
+      <form className="login-form">
+        <input
+          type="text"
+          placeholder="Username"
+          className="login-input"
+          name="username"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="login-input"
+          name="password"
+        />
+        <button type="submit" className="login-button">
+          Sign In
+        </button>
+        <p className="login-footer">
+          Don't have an account? <a href="/register">Register</a>
+        </p>
+
+       
+          Register
+      </form>
+    </div>
+  );
+};
+
+export default Login;
