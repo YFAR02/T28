6c97b8680a79d39a5434aa9d3430ee705716c377
YFAR02 <yfarooqui001@gmail.com>
2025-05-31 08:41:02 -0500

    exam and questions

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,7 @@ import Register from './pages/register.tsx'
 import Login from './components/login/login.tsx'
 import Chat from './pages/Chat.tsx'
 import ChatWindow from './components/chatWindow/chatWindow.tsx'
+import Exam from './pages/exam.tsx' // Adjust the path if your Exam component is located elsewhere
 
 
 function App() { 
@@ -20,6 +21,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/chat" element={<Chat/>} /> {/* Placeholder for Chat page */}
           <Route path="/chat-window" element={<ChatWindow />} /> {/* Placeholder for ChatWindow */}
+          <Route path="/exam" element={<Exam />} /> {/* Placeholder for Exam page */}
           {/* Add more routes as needed */}
         </Routes>
       </div>
