2e4e3cc70d0a73b3411989a6bc88779e8c25ee17
John <lathrop1009@gmail.com>
2025-05-30 21:48:05 -0500

    fixed url

--- backend/Accounts/signals.py
+++ backend/Accounts/signals.py
@@ -3,13 +3,12 @@ from django.conf import settings
 from django_rest_passwordreset.signals import reset_password_token_created
 
 def custom_password_reset_email(sender, instance, reset_password_token, *args, **kwargs):
-    # Customize subject and message as needed
     subject = "Password Reset"
     message = (
         f"Hello,\n\n"
         f"Use the following token to reset your password: {reset_password_token.key}\n\n"
         f"Or click the link below:\n"
-        f"ocalhost:8000/accounts/password_reset/?token={reset_password_token.key}\n\n"
+        f"localhost:8000/accounts/password_reset/?token={reset_password_token.key}\n\n"
         f"Thank you!"
     )
     send_mail(
